a341a01a08c5ed9a9a40672f9b5c09d5ff30e6f0
Toshiyuki Ogura <tos.ogura@gmail.com>
2017-04-20 23:17:41 +0900

    show live or offline status

---
+++ public/js/getmetadata.js
@@ -0,0 +1,46 @@
+angular
+  .module('app', [])
+  .controller('MainController', ['$scope', '$interval', 'Icecast', MainController])
+  .factory('Icecast', ['$http', '$q', Icecast]);
+
+function MainController($scope, $interval, Icecast) {
+  $scope.state = '休止中';
+  var t = $interval(function() {
+    Icecast.getResource().then(function(data) {
+      if (data.icestats !== undefined && data.icestats.source !== undefined) {
+        if ($scope.state === '休止中') {
+          $scope.state = '放送中!';
+        } 
+      } else {
+        if ($scope.state === '放送中!') {
+          $scope.state = '休止中';
+        }
+      }
+    });
+  }, 1000);
+}
+
+function Icecast($http, $q) {
+  var self = {};
+
+  self.getResource = function(){
+    var d = $q.defer();
+    return $http({
+        method: 'GET',
+        url: 'http://live.oguradio.com:8000/status-json.xsl',
+        headers: {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+          'Origin': undefined
+        }
+      })
+      .then(function(response) {
+        d.resolve(response.data);
+        return d.promise;
+      }, function(response) {
+        d.resolve(response);
+        return d.promise;
+      });
+  };
+
+  return self;
+}
